a063706cb7771b30ea2f8d5528dcb4f308a0361f
“trevigui” <guillaume.busson@outlook.com>
2024-02-27 11:10:15 +0100

    modified:   src/Controller/NewsController.php

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -33,8 +33,9 @@ class NewsController extends AbstractController
      */
     public function create(Request $request): Response
     { // C'est toujours obligatoire de spécifier une classe et le type de formualaire renvoyant vers le formulaire créé.
-        $crud = new Articles(); // La classe de l'entity Articles
+        $crud = new Articles(); // La classe de l'entity Articles, je crée une nouvelle entrée dans mon tableau ici, createForm s'occupe de la peupler.
         $form = $this->createForm(CrudType::class, $crud); // Le type a été créé lors de la commande make: //CrudType a été créé dans le dossier Form lorsqu'on make:form. //ne pas oublier d'indiquer l'instaciation de $crud ("sinon erreur Symfony SQL can't be null").
+
         $form->handleRequest($request); // Gestion des erreurs et affichage sur le formulaire directement !
         if ($form->isSubmitted() == true && $form->isValid() == true) {
             $sendDatabase = $this->getDoctrine()->getManager();
@@ -58,7 +59,7 @@ class NewsController extends AbstractController
         $crud = $this->getDoctrine()->getRepository(Articles::class)->find($id); //taxi into repo into entity as class into colonneVar
         $form = $this->createForm(CrudType::class, $crud); // Quand tu ne sais pas quoi mettre comme variable avant une méthode, c'est qu'il faut mettre $this.
         $form->handleRequest($request);
-        if ($form->isSubmitted() == true && $form->isValid() == true) {
+        if ($form->isSubmitted() == true && $form->isValid() == true) { //When you want to check if something is valid, you first have to make sure it was submitted. handlerequest does the former but checking if it is valid is a good idea.
             $sendDatabase = $this->getDoctrine()->getManager();
             $sendDatabase->persist($crud);
             $sendDatabase->flush();
@@ -71,38 +72,43 @@ class NewsController extends AbstractController
         ]);
     }
 
-    /** 
+    /**
      * @Route ("/delete/{id}", name="delete", methods={"GET", "POST"})
+     * Above id is only indicative, it is in no way related to the code below.
      */
     public function delete($id): Response
     {
-        $crud = $this->getDoctrine()->getRepository(Articles::class)->find($id); //taxi into repo into entity into class into colonne
-        $sendDatabase = $this->getDoctrine()->getManager();
-        $sendDatabase->remove($crud);
+        $crud = $this->getDoctrine()->getRepository(Articles::class)->find($id); //in Doctrine, add : repo into entity into class into colonne. this $id is the important one, it's the one stored in the Entity (the actual column)
+        // Pas de handleRequest car il n'y a pas besoin de query la DB, on ne modifie que la doctrine (donc la vraie DB en même temps)
+        $sendDatabase = $this->getDoctrine()->getManager(); //getting the new data into the taxi, manager n'est rien de plus que le taxi dans Doctrine (qui est lui le translation layer vers SQL)
+        $sendDatabase->remove($crud); // remove() au lieu de persist()
         $sendDatabase->flush();
 
-        $this->addFlash('monErreur', 'Soumission réussie !');
+        $this->addFlash('monErreur', 'Suppression réussie !');
         return $this->redirectToRoute('app_news');
     }
+
+
+
+    /**
+     * @Route("news/create2", name="create2", methods={"GET", "POST"})
+     * J'essaie de faire la nouvelle méthode sans getDoctrine()
+     */
+    public function create2(Request $request): Response
+    {
+        $newElement = new Articles();
+        $form = $this->createForm(CrudType::class, $newElement);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $toBeSent = $this->getDoctrine()->getManager();
+            $toBeSent->persist($newElement);
+            $toBeSent->flush();
+
+            $this->addFlash('monErreur', 'Soumission réussie !'); //monErreur sera rappelé plus tard, un addFlash() par route. (Une route C, R U, D, chacune a son propre message addflash(). )
+            return $this->redirectToRoute('app_news');
+        }
+        return $this->render('news/createForm.html.twig', [
+            'form' => $form->createView() // Le createView() est impératif pour afficher les formulaires.
+        ]);
+    }
 }
-
-
-
-
-
-//             Articles.find('title').delete(1)
-
-
-
-//             $toRemove = {title:"0"}
-//             $sendDatabase->remove($toRemove);
-//             $sendDatabase->flush();
-
-//             $this->addFlash('monErreur', 'Soumission réussie !'); //monErreur sera rappelé plus tard, un addFlash() par route. (Une route C, R U, D, chacune a son propre message addflash(). )
-//             return $this->redirectToRoute('app_news');
-//         }
-//         return $this->render('news/createForm.html.twig', [
-//             'form' => $form->createView() // Le createView() est impératif pour afficher les formulaires.
-//         ]);
-//     }
-// }
